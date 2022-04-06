209413b210f1630c9810f3b70428e0df98c8a148
Rail <railbolo@gmail.com>
2022-04-06 19:10:34 +0400

    end lesson1

--- 1/Lesson1/project/js/main.js
+++ 1/Lesson1/project/js/main.js
@@ -1,25 +1,30 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 1000},
-    {id: 2, title: 'Mouse', price: 100},
-    {id: 3, title: 'Keyboard', price: 250},
-    {id: 4, title: 'Gamepad', price: 150},
+  { id: 1, title: "Notebook", price: 1000 },
+  { id: 2, title: "Mouse", price: 100 },
+  { id: 3, title: "Keyboard", price: 250 },
+  { id: 4, title: "Gamepad", price: 150 },
 ];
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить</button>
-              </div>`;
+const renderProduct = (title = "Product", price = 0) => {
+  return `<div class="product-item">
+            <h3>${title}</h3>
+            <p><span class="rub">Р</span> ${price}</p>
+            <button class="by-btn">Добавить</button>
+          </div>`;
 };
 
-const renderProducts = (list) => {
-    const productList = list.map((good) => {
-        return renderProduct(good.title, good.price);
-    });
-    document.querySelector('.products').innerHTML = productList;
+const productsEl = document.querySelector(".products");
 
-    console.log(productList);
+const renderProducts = (list) => {
+  //productsEl.innerHTML = list
+  //  .map((good) => {
+  //    return renderProduct(good.title, good.price);
+  //  })
+  //  .join("");
+  productsEl.innerHTML = list.reduce(
+    (acc, good) => acc + renderProduct(good.title, good.price),
+    ""
+  );
 };
 
 renderProducts(products);
