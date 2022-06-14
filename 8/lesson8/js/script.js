4612cf555707ebfffd09d97d3fd2a65213df871d
Rail <railbolo@gmail.com>
2022-06-14 21:33:56 +0400

    new

--- 8/lesson8/js/script.js
+++ 8/lesson8/js/script.js
@@ -4,6 +4,7 @@ const app = new Vue({
 });
 //console.log(app);
 
+
 let brandOutside = document.querySelector('.brand__outside');
 let catalog = document.querySelector('.menu__catalog');
 let exit = document.querySelector('.catalog__exit');
